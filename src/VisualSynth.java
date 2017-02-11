bf99fa0de30aa35ddffa0b4ed821bad1033f0744
Duncan Hall <duncan.hall@students.olin.edu>
2017-02-11 17:30:16 -0500

    possible template

--- src/VisualSynth.java
+++ src/VisualSynth.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,7 +15,32 @@ public class VisualSynth extends JComponent implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public Dimension getPreferredSize() {
+        return new Dimension(windowSize[0], windowSize[1]);
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // draw
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // update
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Visual FM Synthesizer");
+        VisualSynth vs = new VisualSynth();
+        window.add(vs);
+        window.pack();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        Timer t = new Timer(100, vs);
+        t.start();
+    }
+
 }
